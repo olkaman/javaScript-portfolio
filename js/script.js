e7ef74d098ade4fc935234ced315a91eb50dd0bb
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-09-28 12:18:00 +0200

    Ready linking to articles

--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@
 const links = document.querySelectorAll('.titles a');
 
 function clickHandler(event){
+	event.preventDefault();
 	console.log('The link was clicked!');
 	const clickedElement = this;
   
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
   	const activeLinks = document.querySelectorAll('.titles a.active');
   	for(let activeLink of activeLinks){
   		activeLink.classList.remove('active');
   	}
 
-  /* [INPROGRESS] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
   		clickedElement.classList.add('active');
 
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+  /* remove class 'active' from all articles */
+  	const activeArticles = document.querySelectorAll('.posts article.active');
   	for(let activeArticle of activeArticles){
   		activeArticle.classList.remove('active');
   	}
 
   /* get 'href' attribute from the clicked link */
+  	const hrefAttribute = clickedElement.getAttribute('href');
+  	console.log(hrefAttribute);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  	const desiredArticle = document.querySelector(hrefAttribute); 
+  	console.log(desiredArticle);
 
   /* add class 'active' to the correct article */
+  	desiredArticle.classList.add('active');
 }
 
 for(let link of links){
