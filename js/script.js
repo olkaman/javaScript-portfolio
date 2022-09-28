8fd27bc1d2bff1c03cee47e663ad23f482e0f52f
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-09-28 15:53:26 +0200

    Add JS title links

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const links = document.querySelectorAll('.titles a');
-
 function clickHandler(event){
 	event.preventDefault();
 	console.log('The link was clicked!');
@@ -33,6 +31,46 @@ function clickHandler(event){
   /* add class 'active' to the correct article */
   	desiredArticle.classList.add('active');
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleList(){
+
+	/* remove contents of titleList */
+	const titleList = document.querySelector(optTitleListSelector);
+	titleList.innerHTML = "";
+
+  	/* for each article */
+  	const allArticles = document.querySelectorAll(optArticleSelector);
+  	let html = "";
+  	for(let article of allArticles){
+
+    		/* get the article id */
+    		const ArticleId = article.getAttribute('id');
+
+    		/* find the title element */
+    		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    		/* create HTML of the link */
+    		const linkHTML = '<li><a href="#' + ArticleId + '"><span>' + articleTitle + '</span></a></li>';
+    		
+
+    		/* insert link into titleList */
+
+			html += linkHTML;
+			
+		
+	}
+
+	titleList.innerHTML = html;
+
+}
+generateTitleList();
+
+const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
 	link.addEventListener('click', clickHandler);
