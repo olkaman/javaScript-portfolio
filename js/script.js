68501d2dd68f628d705c37978a12d37ddeb83ae4
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2022-09-28 11:17:48 +0200

    Add for-of loop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const links = document.querySelectorAll('.titles a');
+
+function clickHandler(event){
+	console.log('The link was clicked!');
+	console.log(event);
+  
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+for(let link of links){
+	link.addEventListener('click', clickHandler);
+}
